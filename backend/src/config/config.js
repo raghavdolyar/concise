31dc873e7cb1fc00d69580483d8a8caf3a2e5fd9
raghavdolyar <raghavdolyardell@gmail.com>
2026-09-03 03:51:53 +0530

    fixed cors issue

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'Lax',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   maxAge: 1000 * 60 * 60,
 };
